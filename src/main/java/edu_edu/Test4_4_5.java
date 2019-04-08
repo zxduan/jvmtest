cba16c90dc562265b699b0c39c692d0dbc3a041b
duanzx <15726645490@163.com>
2019-04-08 10:53:21 +0800

    test

---
+++ src/main/java/edu_edu/Test4_4_5.java
@@ -0,0 +1,34 @@
+package edu_edu;
+
+import javax.swing.*;
+import java.awt.*;
+
+/**
+ * 以下程序创建了一个窗口，然后在窗口内显示Hello，World
+ */
+public class Test4_4_5 extends JFrame {
+
+    public static void main(String[] args) throws Exception {
+        new Test4_4_5();
+    }
+
+    public static final int WIDTH = 300;
+    public static final int HEIGHT = 200;
+
+    public Test4_4_5() {
+        setTitle("Hello World");
+        setSize(WIDTH, HEIGHT);
+        Container container = getContentPane();
+        container.add(new TextPanel());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    class TextPanel extends JPanel {
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            g.drawString("Hello,World", 100, 100);
+        }
+    }
+}
---
+++ src/main/java/edu_edu/Test4_5_1.java
@@ -0,0 +1,7 @@
+package edu_edu;
+
+/**
+ * 随着滚动条滚动，给文本框赋值
+ */
+public class Test4_5_1 {
+}
